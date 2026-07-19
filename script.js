5db0b1f069021e4f75595b5553f3b9b52730ddd4
mathieukn229 <mathieukn229@gmail.com>
2026-07-19 15:19:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,25 @@ function sendMessage() {
     }, 1500);
 
                 }
+function startService(service) {
+
+    const input = document.getElementById("userInput");
+
+    if (service === "business") {
+        input.value = "Aide-moi à trouver une idée de business";
+    }
+
+    else if (service === "contenu") {
+        input.value = "Aide-moi à créer du contenu";
+    }
+
+    else if (service === "ia") {
+        input.value = "Apprends-moi l'intelligence artificielle";
+    }
+
+    else if (service === "projet") {
+        input.value = "Aide-moi à développer mon projet";
+    }
+
+    sendMessage();
+}
