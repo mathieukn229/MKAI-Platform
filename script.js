93fd53479b7d84b9c79ba72ffda1aedb7e110ca6
mathieukn229 <mathieukn229@gmail.com>
2026-07-19 13:40:41 +0100

    Add greeting alert on button click

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Bienvenue sur MKAI !");
+// MKAI Platform - Première interaction
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+    alert("🤖 Bonjour ! Je suis MKAI, ton assistant intelligent. La fonction Chat IA arrive bientôt 🚀");
+});
